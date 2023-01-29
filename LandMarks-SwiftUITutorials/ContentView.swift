f200b73cc23b0988435ed39cca46003b81b8602a
domb <woon4910@gmail.com>
2023-01-30 00:20:28 +0900

    feat: Project Init

---
+++ LandMarks-SwiftUITutorials/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LandMarks-SwiftUITutorials
+//
+//  Created by 서동운 on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMarks-SwiftUITutorials/LandMarks_SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarks_SwiftUITutorialsApp.swift
+//  LandMarks-SwiftUITutorials
+//
+//  Created by 서동운 on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarks_SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
