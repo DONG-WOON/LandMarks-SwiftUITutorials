51a517ce73ab7055eddfc3ba5eb70d76eb09a87e
domb <woon4910@gmail.com>
2023-01-30 18:39:07 +0900

    feat: Set Up Navigation

---
+++ LandMarks-SwiftUITutorials/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  LandMarks-SwiftUITutorials
+//
+//  Created by 서동운 on 1/30/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
--- LandMarks-SwiftUITutorials/Views/LandmarkRow.swift
+++ LandMarks-SwiftUITutorials/Views/LandmarkRow.swift
@@ -25,7 +25,12 @@ struct LandmarkRow: View {
 // domb: 여기서 "Missing argument for parameter" 빌드타임에러가 발생하는 이유는 위의 View에 선언한 변수를 제대로 초기화 해주지 않았기 때문이다.
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-//        LandmarkRow()
-        LandmarkRow(landmark: landmarks[0]) // landmarks는 전역변수라 여기서 접근가능
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+                // landmarks는 전역변수라 여기서 접근가능
+            LandmarkRow(landmark: landmarks[1])
+                
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
